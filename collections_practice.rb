b08232bf3862f45c860fc77e60f59c2a72b12a89
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-12 19:54:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,3 +44,13 @@ end
 def sum_array(array)
   array.inject{|sum, n| sum + n}
 end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+  
